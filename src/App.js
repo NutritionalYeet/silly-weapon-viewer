8e7a739d6fb55cfbb74e904754d439c5076fd5a4
NutritionalYeet <audvic12@gmail.com>
2025-02-06 12:13:48 -0800

    Fetch weapons
    The now fetches weapons from a URL, and displays their names onscreen.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+
+const dbUrl = `/chat_app/fetch_weapons.php`; 
+
+const fetchWeapons = async (url) => {
+  try
+  {
+    const response = await fetch(url);
+
+    if (!response.ok)
+      {
+        throw new Error("Nooo! Failed to fetch weapons! :(");
+      }
+
+    const data = await response.json();
+    return Array.isArray(data.weapons) ? data.weapons : [];
+  }
+  catch(error)
+  {
+    console.error("Error fetching weapons: "+error);
+    return[];
+  }
+};
 
 function App() {
+
+  const [items,setItems] = useState([]);
+
+
+  useEffect(()=>{
+    const getItems = async () =>
+    {
+      const fetchedItems = await fetchWeapons(dbUrl);
+      setItems(Array.isArray(fetchedItems) ? fetchedItems : []);
+    };
+
+    getItems();
+
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <div>
+          <h1>Weapons List</h1>
+          <ul>
+            {items.map((item) =>(
+              <li key = {item.id}>{item.name}</li>
+            ))}
+          </ul>
+        </div>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
