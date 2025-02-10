bd213067fb9b8c64ced6f8de444a4b77148c5453
NutritionalYeet <audvic12@gmail.com>
2025-02-09 18:12:34 -0800

    Separate columns
    Adjusted base layout.

--- src/App.js
+++ src/App.js
@@ -44,28 +44,40 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div id="mainRow">
+          <div id="weapon-selector">
+            <div>
+                <h1>Weapons List</h1>
+                <ul>
+                  {items.map((item) =>(
+                    <li key = {item.id}><button>{item.name}</button></li>
+                  ))}
+                </ul>
+              </div>
+          </div>
+          <div id="content">
+            <img src={logo} className="App-logo" alt="logo" />
+            
+            <div>
+                <h1>Title Here</h1>
+            </div>
 
-        <div>
-          <h1>Weapons List</h1>
-          <ul>
-            {items.map((item) =>(
-              <li key = {item.id}>{item.name}</li>
-            ))}
-          </ul>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+
+          </div>
         </div>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
