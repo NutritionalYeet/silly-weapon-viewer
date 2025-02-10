1dd208c03e6ba32c721ac1a923a55fe15cd54228
NutritionalYeet <audvic12@gmail.com>
2025-02-10 12:46:04 -0800

    Added stats
    Displays more stats. Assumptions are made about specific fields - should be broken up to allow for easier integration of different stat sets.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,33 @@ import { useEffect, useState } from "react";
 //
 const dbUrl = `/chat_app/fetch_weapons.php`; 
 
+
 class Weapon
 {
-  constructor(id,n,d)
+  constructor(id,name,description, quality, sl, price, stamina,
+    nerve, speed, charge, melee_damage, ranged_damage, spell_damage,
+    healing, gouge, resilience, guard, dominance
+  )
   {
     this.id = id
-    this.name = n;
-    this.description = d;
+    this.name = name;
+    this.description = description;
+    this.quality = quality;
+    this.sl = sl;
+    this.price = price;
+    this.stamina = stamina;
+
+    this.nerve = nerve;
+    this.speed = speed;
+    this.charge = charge;
+    this.melee_damage = melee_damage;
+    this.ranged_damage = ranged_damage
+    this.spell_damage = spell_damage;
+    this.healing = healing;
+    this.gouge = gouge;
+    this.resilience = resilience;
+    this.guard = guard; 
+    this.dominance = dominance;
   }
 }
 
@@ -32,7 +52,12 @@ const fetchWeapons = async (url) => {
     const data = await response.json();
 
     const weapons = Array.isArray(data.weapons) 
-      ? data.weapons.map(weapon => new Weapon(weapon.id, weapon.name,weapon.description))
+      ? data.weapons.map(weapon => 
+        new Weapon(weapon.id, weapon.name, weapon.description, weapon.quality,
+          weapon.sl, weapon.price, weapon.stamina, weapon.nerve, weapon.speed, weapon.charge,
+          weapon.melee_damage, weapon.ranged_damage, weapon.spell_damage, weapon.healing, weapon.gouge,
+          weapon.resilience, weapon.guard,weapon.dominance
+        ))
       : [];
 
     currentWeapon = weapons.length > 0? weapons[0] : null;
@@ -85,13 +110,82 @@ function App() {
           <div id="content">
 
 
-
-
             {
               currentWeapon ? 
               <div id ="weapon-info-grid">
                 <h1>{currentWeapon.name}</h1>
-                <p>{currentWeapon.description}</p> 
+
+                <div class = "stat-column-container">
+                  <div class = "stat-column">
+                    <div class = "stat-row">
+                      <p class = "stat-label">id</p><p class ="stat">{currentWeapon.id}</p>
+                      
+                    </div>
+                    <div class = "stat-row">
+                      <p class = "stat-label">level</p><p class ="stat">{currentWeapon.sl}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">stamina</p><p class ="stat">{currentWeapon.stamina}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">speed</p><p class ="stat">{currentWeapon.speed}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">melee dmg</p><p class ="stat">{currentWeapon.melee_damage}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">spell dmg</p><p class ="stat">{currentWeapon.spell_damage}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">gouge</p><p class ="stat">{currentWeapon.gouge}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">guard</p><p class ="stat">{currentWeapon.guard}</p>
+                    </div>
+                  </div>
+
+                  <div class = "stat-column">
+                    <div class = "stat-row">
+                      <p class = "stat-label">quality</p><p class ="stat">{currentWeapon.quality}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">price</p><p class ="stat">{currentWeapon.price}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">nerve</p><p class ="stat">{currentWeapon.nerve}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">charge</p><p class ="stat">{currentWeapon.charge}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">ranged dmg</p><p class ="stat">{currentWeapon.ranged_damage}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">healing</p><p class ="stat">{currentWeapon.healing}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">resilience</p><p class ="stat">{currentWeapon.resilience}</p>
+                    </div>
+
+                    <div class = "stat-row">
+                      <p class = "stat-label">dominance</p><p class ="stat">{currentWeapon.dominance}</p>
+                    </div>
+                  </div>
+                </div>
+
+                <p>"{currentWeapon.description}"</p> 
               </div>
                 : <img src={logo} className="App-logo" alt="logo" />
             }
